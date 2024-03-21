e6f15f1e2b55f4b9a10e8c80a6a0841d817297ee
Nico <lim.nicolebenedict3@gmail.com>
2024-03-21 11:52:54 +0800

    feat: integrate custom post type products and acf fields

--- src/views/blocks/featured-products/featured-products.php
+++ src/views/blocks/featured-products/featured-products.php
@@ -17,6 +17,42 @@
     $theme = get_field('theme') ?: 'light';
     $background = get_field('background_color') ?: 'transparent';
     $content = get_field('content');
+    $button = get_field('button');
+    $bottom_content = get_field('bottom_content');
+
+    // $post_per_page = 9;
+    $query_helper = new \SDEV\Helper\Query();
+    $tax_query = ['relation' => 'and'];
+    $posts = $query_helper->setQueryArgs([
+        'post_type' => 'cpt_products',
+        'post_status' => 'publish',
+        'orderby' => 'date',
+        'meta_query' => [
+            [
+                'key' => 'featured',
+                'value' => true
+            ]
+        ]
+    ]); 
+    $posts->setOrder('date', 'desc');
+    $posts->setPageSize(-1);
+    $posts->setPage(1);
+    $products = $posts->getList();
+    $product_results = [];
+
+    if($products) {
+
+        foreach($products as $product) {
+            $post_id = $product->ID;
+            
+            $product_results[$post_id] = [
+                'title' => $product->post_title,
+                'image' => get_the_post_thumbnail_url($post_id),
+                'excerpt' => get_the_excerpt($post_id)
+            ];
+
+        }
+    }
 
     // Create class attribute allowing for custom "className" and "align" values.
     $class_name = 'block-featured-products';
@@ -39,83 +75,48 @@
 
     <section class="block--custom-layout <?= $class_name ?>" <?= $anchor ?> style="background-color:<?= $background ?>;" data-ux="featured-products">
         <div class="block-featured-products__wrapper">
-            <a href="#" class="site-btn sm border-none block-featured-products__btn">Products</a>
+            <?php if($button):?>
+                <a href="<?= $button['url']?>" target="<?= $button['target']?>" class="site-btn sm border-none block-featured-products__btn"><?= $button['title']?></a>
+            <?php endif;?>
+           
+            <?php if($product_results):
+                $count = 1;    
+            ?>
             <div class="block-featured-products__slider-wrapper">
                 <div class="block-featured-products__slider owl-carousel">
 
-                    <div class="block-featured-products__slide">
-                        <div class="block-featured-products__product">
-                            <div class="block-featured-products__product-img canvas-img">
-                                <canvas width="238" height="196"></canvas>
-                                <img src="/wp-content/uploads/2024/03/fp-1.png" alt="Product">
-                            </div>
-                            <div class="block-featured-products__product-content">
-                                <div class="block-featured-products__product-content-inner">
-                                    <h6>Galvanised Steel</h6>
-                                    <p>Steel coated with zinc to enhance its corrosion resistance and  durability</p>
+                    <?php foreach($product_results as $product):?>
+                        <div class="block-featured-products__slide">
+                            <div class="block-featured-products__product">
+                                <div class="block-featured-products__product-img canvas-img">
+                                    <canvas width="238" height="196"></canvas>
+                                    <img src="<?=  $product['image']?>" alt="<?=  $product['title']?>">
                                 </div>
-                                <div class="block-featured-products__product-count">1 of 10</div>
+                                <div class="block-featured-products__product-content">
+                                    <div class="block-featured-products__product-content-inner">
+                                        <h6><?=  $product['title']?></h6>
+                                        <p><?=  $product['excerpt']?></p>
+                                    </div>
+                                    <div class="block-featured-products__product-count"><?= $count?> of <?= count($product_results)?></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div class="block-featured-products__slide">
-                        <div class="block-featured-products__product">
-                            <div class="block-featured-products__product-img canvas-img">
-                                <canvas width="238" height="196"></canvas>
-                                <img src="/wp-content/uploads/2024/03/fp-2.png" alt="Product">
-                            </div>
-                            <div class="block-featured-products__product-content">
-                                <div class="block-featured-products__product-content-inner">
-                                    <h6>Galvanised Steel</h6>
-                                    <p>Steel coated with zinc to enhance its corrosion resistance and  durability</p>
-                                </div>
-                                <div class="block-featured-products__product-count">1 of 10</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="block-featured-products__slide">
-                        <div class="block-featured-products__product">
-                            <div class="block-featured-products__product-img canvas-img">
-                                <canvas width="238" height="196"></canvas>
-                                <img src="/wp-content/uploads/2024/03/placeholder-2.jpg" alt="Product">
-                            </div>
-                            <div class="block-featured-products__product-content">
-                                <div class="block-featured-products__product-content-inner">
-                                    <h6>Galvanised Steel</h6>
-                                    <p>Steel coated with zinc to enhance its corrosion resistance and  durability</p>
-                                </div>
-                                <div class="block-featured-products__product-count">1 of 10</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="block-featured-products__slide">
-                        <div class="block-featured-products__product">
-                            <div class="block-featured-products__product-img canvas-img">
-                                <canvas width="238" height="196"></canvas>
-                                <img src="/wp-content/uploads/2024/03/placeholder-2.jpg" alt="Product">
-                            </div>
-                            <div class="block-featured-products__product-content">
-                                <div class="block-featured-products__product-content-inner">
-                                    <h6>Galvanised Steel</h6>
-                                    <p>Steel coated with zinc to enhance its corrosion resistance and  durability</p>
-                                </div>
-                                <div class="block-featured-products__product-count">1 of 10</div>
-                            </div>
-                        </div>
-                    </div>
+                    <?php $count++; endforeach;?>
+                    
 
                 </div>
+
                 <div class="block-featured-products__controls owl-carousel-controls">
-                    <!-- <button class="slide-prev-button" aria-label="Prev"><i class="fas fa-arrow-left"></i></button>
-                    <button class="slide-next-button" aria-label="Next"><i class="fas fa-arrow-right"></i></button> -->
+                    <!-- arrows goes here -->
                 </div>
+
             </div>
-            <div class="block-featured-products__content">
-                <p>Explore our <a href="http://east-coast-steel.local/products/">Products</a></p>
-            </div>
+            <?php endif;?>
+            <?php if($bottom_content):?>
+                <div class="block-featured-products__content">
+                    <?= $bottom_content?>
+                </div>
+            <?php endif;?>
         </div>
     </section>
 
