d7bbc55258cfe595a37010e6f46e0f47c779b82e
Nico <lim.nicolebenedict3@gmail.com>
2024-03-22 08:53:42 +0800

    feat: created desktop, tablet and mobile view layout

--- src/views/partials/footer-lower.php
+++ src/views/partials/footer-lower.php
@@ -9,29 +9,20 @@
     $footer_logo = get_field('footer_logo_image', 'options');
 
 ?>
-<div class="pf-lower container-block">
-    <div class="tb-wr">
-        <div class="tb-c-wr content-cell">
-            <div class="tb-wr">
-                <?php if($footer_logo) : ?>
-                    <div class="tb-c-wr footer-logo-wrap">
-                        <img src="<?= $footer_logo['url'] ?>" alt="<?= $footer_logo['alt'] ?>" />
-                    </div>
-                <?php endif; ?>
-                <div class="tb-c-wr footer-copy-wrap">
-                    <?= get_field('footer_main_copy', 'options')  ?>
-                </div>
-            </div>
+<div class="pf-lower">
+    <div class="pf-container">
+        <div class="pf-logo">
+            <a href="#">
+                <img src="<?= $footer_logo['url']?>" alt="Logo" class="pf-logo-default">
+                <img src="/wp-content/uploads/2024/03/logo-footer-mobile.svg" alt="" class="pf-logo-secondary">
+            </a>
         </div>
-        <div class="tb-c-wr copyright-cell">
-            <div class="tb-wr">
-                <div class="tb-c-wr">
-                    <?= get_field('copyright_line', 'options') ?>
-                </div>
-                <div class="tb-c-wr branding-cell">
-                    <?= get_field('branding_line', 'options') ?>
-                </div>
-            </div>
+        <div class="pf-copyright">
+            <ul>
+                <li>All rights reserved © 2024 East Coast Steel</li>
+                <li><a href="#">Policies</a></li>
+                <li><a href="#">Terms and conditions</a></li>
+            </ul>
         </div>
     </div>
 </div>
--- src/views/partials/footer-upper.php
+++ src/views/partials/footer-upper.php
@@ -11,39 +11,62 @@
     $email = get_field('gen_email_address', 'options');
     $location = get_field('gen_location', 'options');
     $icon_class = (defined('FOOTER_ALT_CLASS')) ? '-light' : '';
+    $footer_contact_title = get_field('footer_contact_title', 'options');
+    $footer_contact_details = get_field('footer_contact_details', 'options');
+
+    $social_media = [
+        [
+            'icon' => '<i class="fab fa-facebook"></i>',
+            'url' => get_field('sl_facebook') ? get_field('sl_facebook') : '#'
+        ],
+        [
+            'icon' => '<i class="fab fa-instagram-square"></i>',
+            'url' => get_field('sl_instagram') ? get_field('sl_instagram') : '#'
+        ],
+        [
+            'icon' => '<i class="fab fa-linkedin"></i>',
+            'url' => get_field('sl_linked_in') ? get_field('sl_linked_in') : '#'
+        ]
+    ];
 ?>
-<div class="pf-upper container-block">
-    <div id="get-in-touch">
-        <div class="git-heading">
-            <h3><?= get_field('get_in_touch_heading', 'options')  ?></h3>
-            <h5><?= get_field('get_in_touch_subheading', 'options')  ?></h5>
-        </div>
-        <div class="git-details">
-            <div class="tb-wr">
-                <div class="tb-c-wr spacer-cell"></div>
-                <div class="tb-c-wr phone-cell">
-                    <?php if($phone) : ?>
-                        <a href="tel:<?= $phone ?>" target="_blank">
-                            <img src="<?= \SDEV\Utils::getThemeResourcePath('assets/images/phone-icon'.$icon_class.'.svg') ?>" alt="Phone Icon" />
-                            <span><?= $phone ?></span>
-                        </a>
-                    <?php endif; ?>
-                </div>
-                <div class="tb-c-wr email-cell">
-                    <?php if($email) : ?>
-                        <a href="mailto:<?= $email ?>" target="_blank">
-                            <img src="<?= \SDEV\Utils::getThemeResourcePath('assets/images/email-icon'.$icon_class.'.svg') ?>" alt="E-mail Icon" />
-                            <span><?= $email ?></span>
-                        </a>
-                    <?php endif; ?>
-                </div>
-                <div class="tb-c-wr location-cell">
-                    <?php if($location) : ?>
-                        <img src="<?= \SDEV\Utils::getThemeResourcePath('assets/images/location-pin-icon'.$icon_class.'.svg') ?>" alt="Location Pin Icon" />
-                        <span><?= $location ?></span>
-                    <?php endif; ?>
-                </div>
-                <div class="tb-c-wr spacer-cell"></div>
+<div class="pf-upper">
+    <div class="pf-container">
+        <div class="pf-row">
+            <div class="pf-col pf-contact">
+                <p class="pf-title">General enquiries</p>
+                <ul>
+                    <li><a href="tel:02 9618 6266">02 9618 6266</a></li>
+                    <li><a href="mailto:sales@eastcoaststeel.com.au">sales@eastcoaststeel.com.au</a></li>
+                </ul>
+                <a href="#" class="pf-btn">
+                    Contact us 
+                    <span class="slide-next-button sm pf-btn-arrow">
+                        <i class="fas fa-arrow-right"></i>
+                    </span>
+                </a>
+            </div>
+            <div class="pf-col pf-menu">
+                <?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'menu_id' => 'pf-nav', 'theme_location' => 'secondary-menu', 'menu_class' => 'pf-nav') ); ?>
+            </div>
+            <div class="pf-col pf-address">
+                <p class="pf-title">Visit us</p>
+                <p><a href="#" target="_blank">4 Moorlands Road, Ingleburn,<br> NSW 2565</a></p>
+            </div>
+            <div class="pf-col pf-smi">
+                <p class="pf-title">Follow us on</p>
+                <?php if($social_media):?>
+                    <ul class="site-smi">
+                        <?php foreach($social_media as $smi):?>
+                            <?php if($smi):?>
+                                <li>
+                                    <a href="<?= $smi['url']?>" target="_blank" aria-label="social media">
+                                        <?= $smi['icon']?>
+                                    </a>
+                                </li>
+                            <?php endif;?>
+                        <?php endforeach;?>
+                    </ul>
+                <?php endif;?>
             </div>
         </div>
     </div>
